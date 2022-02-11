014bca6355a4263c623ab2f7c58714e6302460b2
DongHyeon Park <paramore@kakao.com>
2022-02-11 17:50:39 +0900

    Ch20 Challenge ongoing

--- Beatbox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
+++ Beatbox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.kt
@@ -13,6 +13,7 @@ private const val MAX_SOUNDS = 5
 class BeatBox(private val assets: AssetManager) {
 
     val sounds: List<Sound>
+    var playBackSpeed: Float = 1.0f
     private val soundPool = SoundPool.Builder()
         .setMaxStreams(MAX_SOUNDS)
         .build()
@@ -23,7 +24,7 @@ class BeatBox(private val assets: AssetManager) {
 
     fun play(sound:Sound){
         sound.soundId?.let{
-            soundPool.play(it, 1.0f, 1.0f, 1, 0, 1.0f)
+            soundPool.play(it, 1.0f, 1.0f, 1, 0, playBackSpeed)
         }
     }
 
