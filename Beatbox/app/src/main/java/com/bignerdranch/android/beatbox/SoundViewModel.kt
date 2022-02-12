40eb7a786395b427360f755e7ab55eb0b661e40d
DongHyeon Park <paramore@kakao.com>
2022-02-12 18:53:59 +0900

    Ch20 Challenge ongoing (2)

--- Beatbox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
+++ Beatbox/app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -4,6 +4,14 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 
 class SoundViewModel(private val beatBox:BeatBox) : BaseObservable() {
+
+    fun onSeekbarChanged(progress:Int){
+        playBackSpeed = progress.toFloat() / 100
+        sound?.let {
+            beatBox.playWithRate(it, playBackSpeed)
+        }
+    }
+
     fun onButtonClicked() {
         sound?.let{
             beatBox.play(it)
@@ -20,4 +28,10 @@ class SoundViewModel(private val beatBox:BeatBox) : BaseObservable() {
     val title: String?
         get() = sound?.name
 
+    @get:Bindable
+    var playBackSpeed: Float = 1.0f
+        set(speed) {
+            field = speed
+            notifyChange()
+        }
 }
