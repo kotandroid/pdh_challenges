9f3fc187908c2b33a2d33f43ed27a113e8c4f98f
DongHyeon Park <paramore@kakao.com>
2022-02-16 01:13:09 +0900

    Ch24 Ended

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -4,11 +4,12 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import com.bignerdranch.android.photogallery.api.PhotoResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 private const val TAG = "FlickrFetchr"
 
@@ -17,24 +18,32 @@ class FlickrFetchr {
 
     init {
         val retrofit:Retrofit = Retrofit.Builder()
-            .baseUrl("https://www.flickr.com")
-            .addConverterFactory(ScalarsConverterFactory.create())
+            .baseUrl("https://api.flickr.com/")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
-    fun fetchContents():LiveData<String> {
-        val responseLiveData: MutableLiveData<String> = MutableLiveData()
-        val flickrRequest: Call<String> = flickrApi.fetchContents()
+    fun fetchPhotos():LiveData<List<GalleryItem>> {
+        val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
+        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
 
-        flickrRequest.enqueue(object: Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
+        flickrRequest.enqueue(object: Callback<FlickrResponse> {
+            override fun onResponse(call: Call<FlickrResponse>, response: Response<FlickrResponse>) {
                 Log.d(TAG, "Response received")
-                responseLiveData.value = response.body()
+                // 파싱
+                val flickrResponse:FlickrResponse? = response.body()
+                val photoResponse:PhotoResponse? = flickrResponse?.photos
+                var galleryItems:List<GalleryItem> = photoResponse?.galleryItems
+                    ?: mutableListOf()
+                galleryItems = galleryItems.filterNot {
+                    it.url.isBlank()
+                }
+                responseLiveData.value = galleryItems
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
                 Log.e(TAG, "Failed to fetch photos", t)
             }
         })
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,10 +1,15 @@
 package com.bignerdranch.android.photogallery.api
 
+import com.bignerdranch.android.photogallery.FlickrResponse
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlickrApi {
 
-    @GET("/")
-    fun fetchContents(): Call<String>
+    @GET("services/rest/?method=flickr.interestingness.getList" +
+    "&api_key=b060e3cca52b23e160caf055b92fa60e" +
+    "&format=json" +
+    "&nojsoncallback=1" +
+    "&extras=url_s")
+    fun fetchPhotos():Call<FlickrResponse>
 }
