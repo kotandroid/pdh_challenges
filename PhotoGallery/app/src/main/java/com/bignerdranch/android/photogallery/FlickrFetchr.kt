8fbb05e4c65d589985eac8477aa1139347cc6b1d
DongHyeon Park <paramore@kakao.com>
2022-02-23 18:30:44 +0900

    Ch26 Ended

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.kt
@@ -23,17 +23,29 @@ class FlickrFetchr {
     private val flickrApi: FlickrApi
 
     init {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit:Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
     fun fetchPhotos():LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.fetchPhotos())
+    }
+
+    fun searchPhotos(query: String):LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.searchPhotos(query))
+    }
+
+    private fun fetchPhotoMetadata(flickrRequest: Call<FlickrResponse>): LiveData<List<GalleryItem>> {
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
 
         flickrRequest.enqueue(object: Callback<FlickrResponse> {
             override fun onResponse(call: Call<FlickrResponse>, response: Response<FlickrResponse>) {
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -4,17 +4,17 @@ import com.bignerdranch.android.photogallery.FlickrResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
 
-    @GET("services/rest/?method=flickr.interestingness.getList" +
-    "&api_key=b060e3cca52b23e160caf055b92fa60e" +
-    "&format=json" +
-    "&nojsoncallback=1" +
-    "&extras=url_s")
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos():Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query:String): Call<FlickrResponse>
 }
