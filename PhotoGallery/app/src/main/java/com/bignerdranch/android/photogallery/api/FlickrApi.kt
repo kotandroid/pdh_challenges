1f6cab14210cbf99e3edb30f47bceda01359b209
DongHyeon Park <paramore@kakao.com>
2022-02-23 16:57:45 +0900

    Ch25 Ended

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -1,8 +1,10 @@
 package com.bignerdranch.android.photogallery.api
 
 import com.bignerdranch.android.photogallery.FlickrResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
 
@@ -12,4 +14,7 @@ interface FlickrApi {
     "&nojsoncallback=1" +
     "&extras=url_s")
     fun fetchPhotos():Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
