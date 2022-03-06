73fa46da13848477e3d45eb167e79aa874cf0284
DongHyeon Park <paramore@kakao.com>
2022-03-06 18:14:42 +0900

    Ch28 Ended

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/NotificationReceiver.kt
@@ -1,15 +1,26 @@
 package com.bignerdranch.android.photogallery
 
+import android.app.Activity
+import android.app.Notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationManagerCompat
 
 private const val TAG = "NotificationReceiver"
 
 class NotificationReceiver:BroadcastReceiver() {
 
-    override fun onReceive(p0: Context, p1: Intent) {
-        Log.i(TAG, "received broadcast: ${p1.action}")
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.i(TAG, "received result: $resultCode")
+        if (resultCode != Activity.RESULT_OK){
+            return // 액티비티가 foreground에 있으면 취소
+        }
+
+        val requestCode = intent.getIntExtra(PollWorker.REQUEST_CODE, 0)
+        val notification:Notification = intent.getParcelableExtra(PollWorker.NOTIFICATION)!!
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(requestCode, notification)
     }
 }
