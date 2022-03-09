95ba55e72f5c017497cf284900150bd47e50d764
DongHyeon Park <paramore@kakao.com>
2022-03-09 23:21:09 +0900

    Ch31 ongoing

---
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.sunset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sceneView:View
+    private lateinit var sunView:View
+    private lateinit var skyView:View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sceneView = findViewById(R.id.scene)
+        sunView = findViewById(R.id.sun)
+        skyView = findViewById(R.id.sky)
+    }
+}
