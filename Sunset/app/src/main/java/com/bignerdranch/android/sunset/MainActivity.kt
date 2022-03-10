886f622bd155c6e4543faa63b70d1d36d028ae8c
DongHyeon Park <paramore@kakao.com>
2022-03-10 20:38:32 +0900

    Ch31 Challenge 3 Ended

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.res.ColorStateList
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.graphics.drawable.ShapeDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,14 +13,16 @@ import android.view.animation.AccelerateInterpolator
 import android.view.animation.Animation
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var shouldRise:Boolean = false
+    private var isRising:Boolean = true
     private lateinit var sceneView:View
     private lateinit var sunView:View
     private lateinit var skyView:View
     private lateinit var burningAnimator: ValueAnimator
+    private lateinit var animatorSet:AnimatorSet
 
     private val blueSkyColor: Int by lazy {
         ContextCompat.getColor(this, R.color.blue_sky)
@@ -49,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         sunView = findViewById(R.id.sun)
         skyView = findViewById(R.id.sky)
         burningAnimator = generateBurning()
+        animatorSet = AnimatorSet()
 
         burningAnimator.start()
         sceneView.setOnClickListener {
@@ -60,7 +64,7 @@ class MainActivity : AppCompatActivity() {
     private fun generateBurning():ValueAnimator{
         val burningAnimator = ValueAnimator
             .ofInt(brightSunColor, burningSunColor)
-            .setDuration(1000)
+            .setDuration(500)
         burningAnimator.repeatCount = ValueAnimator.INFINITE
         burningAnimator.repeatMode = ValueAnimator.REVERSE
         burningAnimator.setEvaluator(ArgbEvaluator())
@@ -75,53 +79,35 @@ class MainActivity : AppCompatActivity() {
         val sunYstart = sunView.top.toFloat()
         val sunYEnd = skyView.height.toFloat()
 
-        if (!shouldRise){
-            val heightAnimator = ObjectAnimator
-                .ofFloat(sunView, "y", sunYstart, sunYEnd)
-                .setDuration(3000)
+        val heightAnimator = ObjectAnimator
+            .ofFloat(sunView, "y", sunYstart, sunYEnd)
+            .setDuration(1500)
 
-            heightAnimator.interpolator = AccelerateInterpolator()
+        heightAnimator.interpolator = AccelerateInterpolator()
 
-            val sunsetSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
-                .setDuration(3000)
-            sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(1500)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
 
-            val nightSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", sunsetSkyColor, nightSkyColor)
-                .setDuration(1500)
-            nightSkyAnimator.setEvaluator(ArgbEvaluator())
+        val nightSkyAnimator = ObjectAnimator
+            .ofInt(skyView, "backgroundColor", sunsetSkyColor, nightSkyColor)
+            .setDuration(750)
+        nightSkyAnimator.setEvaluator(ArgbEvaluator())
 
-            val animatorSet = AnimatorSet()
-            animatorSet.play(heightAnimator)
-                .with(sunsetSkyAnimator) // 같이 작동하는 애니메이션
-                .before(nightSkyAnimator) // 이 애니메이션 전에 작동하기
+        animatorSet.play(heightAnimator)
+            .with(sunsetSkyAnimator) // 같이 작동하는 애니메이션
+            .before(nightSkyAnimator) // 이 애니메이션 전에 작동하기
+
+        if (isRising){
             animatorSet.start()
-            shouldRise = true
+            isRising = false
         } else {
-            val heightAnimator = ObjectAnimator
-                .ofFloat(sunView, "y", sunYEnd, sunYstart)
-                .setDuration(3000)
-
-            heightAnimator.interpolator = AccelerateInterpolator()
-
-            val sunsetSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", sunsetSkyColor, blueSkyColor)
-                .setDuration(3000)
-            sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
-
-            val nightSkyAnimator = ObjectAnimator
-                .ofInt(skyView, "backgroundColor", nightSkyColor, sunsetSkyColor)
-                .setDuration(1500)
-            nightSkyAnimator.setEvaluator(ArgbEvaluator())
-
-            val animatorSet = AnimatorSet()
-            animatorSet.play(nightSkyAnimator)
-                .with(heightAnimator) // 같이 작동하는 애니메이션
-                .before(sunsetSkyAnimator) // 이 애니메이션 전에 작동하기
-                .before(burningAnimator)
-            animatorSet.start()
-            shouldRise = false
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                animatorSet.reverse()
+            }
+            burningAnimator.start()
+            isRising = true
         }
     }
 }
